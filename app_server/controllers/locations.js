c0589fb4ab847e6fc9390e50bf203fe4b5a17cd0
hasan.sayeed <hasan.sayeed@bjitgroup.com>
2018-06-05 17:11:57 +0600

    new layout for info and home page

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -1,14 +1,14 @@
 // Get Home Page
 module.exports.homeList = function(req, res){
-    res.render('index', {title: 'Home'});
+    res.render('locations-list', {title: 'Home'});
 };
 
 // Get Location Info
 module.exports.locationInfo = function(req, res){
-    res.render('index', {title: 'Location Info'});
+    res.render('locations-info', {title: 'Location Info'});
 }
 
 // Get Review page
 module.exports.addReview = function(req, res){
-    res.render('index', {title: 'Add review'});
+    res.render('add-review', {title: 'Add review'});
 };
