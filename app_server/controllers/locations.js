5d92af443573ba4437b9cfe08ec3b60fe806de65
hasan.sayeed <hasan.sayeed@bjitgroup.com>
2018-06-07 17:14:07 +0600

    data model added

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -1,30 +1,29 @@
-// Get Home Page
-module.exports.homeList = function(req, res){
+module.exports.homeList = function(req, res) {
     res.render('locations-list', {
         title: 'Loc8r - find a place to work with wifi',
-        pageHeader:{
+        pageHeader: {
             title: 'Loc8r',
             strapline: 'Find places to work with wifi near you!'
         },
-        locations:[{
+        locations: [{
             name: 'Starcups',
             address: '125 High Street, Reading, RG6 1PS',
             rating: 3,
-            facilities: ['Hot drinks','Food','Premium wifi'],
+            facilities: ['Hot drinks', 'Food', 'Premium wifi'],
             distance: '100m'
-        },{
+        }, {
             name: 'Cafe Hero',
             address: '125 High Street, Reading, RG6 1PS',
             rating: 4,
             facilities: ['Hot drinks', 'Food', 'Premium wifi'],
             distance: '200m'
-        },{
+        }, {
             name: 'Burger Queen',
             address: '125 High Street, Reading, RG6 1PS',
             rating: 2,
             facilities: ['Food', 'Premium wifi'],
             distance: '250m'
-        },{
+        }, {
             name: 'KaFC',
             address: '125 High Street, Reading, RG6 1PS',
             rating: 3,
@@ -32,14 +31,12 @@ module.exports.homeList = function(req, res){
             distance: '350m'
         }]
     });
-};
-
-// Get Location Info
-module.exports.locationInfo = function(req, res){
-    res.render('locations-info', {title: 'Location Info'});
 }
 
-// Get Review page
-module.exports.addReview = function(req, res){
-    res.render('review-form', {title: 'Add review'});
-};
+module.exports.locationInfo = function(req, res) {
+    res.render('locations-info', { title: 'Location Info' });
+}
+
+module.exports.addReview= function(req, res) {
+    res.render('review-form', { title: 'Add review' });
+}
