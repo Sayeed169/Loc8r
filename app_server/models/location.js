cfc15b181a04b385cc81c798a30576bef403be10
hasan.sayeed <hasan.sayeed@bjitgroup.com>
2018-06-12 15:05:05 +0600

    database schema ready and hosted

--- app_server/models/location.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require('mongoose');
-
-var locationSchema = new mongoose.Schema({
-    name: {type:String, required:true},
-    address: {type:String, default:0, min:0, max:5},
-    rating: Number,
-    facilities:[String],
-    coords:{type:[Number], index:'2dsphere'}
-});
