2fb755ae086675494fe36d7920d451777d7bb72b
hasan.sayeed <hasan.sayeed@bjitgroup.com>
2018-07-04 13:54:37 +0600

    api development

---
+++ app_api/controllers/reviews.js
@@ -0,0 +1,21 @@
+var mongoose = require('mongoose');
+var review = mongoose.model('Location')
+
+// var sendJsonResponse = function(res, status, content){
+//     res.status(status);
+//     res.content(content);
+// };
+
+// module.exports.reviewsCreate = function(req, res){
+//     sendJsonResponse(res, 200, {"status":"success"});
+// }
+
+// module.exports.reviewReadOne = function(req, res){
+//     sendJsonResponse(res, 200, {"status":"success"});
+// }
+// module.exports.reviewsUpdateOne = function(req, res){
+//     sendJsonResponse(res, 200, {"status":"success"});
+// }
+// module.exports.reviewsDeleteOne = function(req, res){
+//     sendJsonResponse(res, 200, {"status":"success"});
+// }
